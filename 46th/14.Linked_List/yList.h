c53301573a99a110bdaef9712a68f360d7ff1dee
Mooooon-s <mmk990212@naver.com>
2023-01-16 21:43:04 +0900

    01/16

--- 46th/14.Linked_List/yList.h
+++ 46th/14.Linked_List/yList.h
@@ -72,6 +72,35 @@ void DeleteNode(int data) {
 //삽입
 //숙제
 
+//노드 삽입
+void InsertNode(int data, int data1) {
+	NODE* p = find(data1);		//data1앞에 노드를 추가하기 위해서 data1의 노드를 찾음
+	NODE* nextP = p;
+	NODE* prevP = p->_prev;
+	if (p->_prev == nullptr) {		//최초의 노드 앞에 추가 하려고 할 때
+		head = p->_prev = new NODE;	//노드의 앞에 노드 생성(최초의 노드 앞에 생성하기 때문에 최초의 노드를 초기화)
+		p->_prev->_data = data;		//생성된 노드에 값 입력
+		p->_prev->_next = p;		//생성된 노드와 다음 노드를 연결
+	}
+	else {
+		p->_prev = new NODE;		//노드의 앞에 노드 생성
+		p->_prev->_data = data;		//생성된 노드에 값 입력
+		p->_prev->_next = p;		//생성된 노드와 다음 노드를 연결
+
+	}
+
+	if (prevP != nullptr) {
+		prevP->_next = p->_prev;	//생성된 노드의 앞의 노드와 연결
+		p->_prev->_prev = prevP;	//생성된 노드의 앞의 노드와 생성된 노드를 연결
+	}
+	else {							//최초의 노드 앞에 추가하려고 할 때
+		p->_prev->_prev = nullptr;	//생성된 노드 앞의 노드가 없음
+	}
+}
+
+
+
+
 //메모리 해제
 void ReleaseNode() {
 	NODE* p= tail;
