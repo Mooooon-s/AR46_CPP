4898fa054f2b57b40871f3ac0482fb07a19eb8ba
Mooooon-s <mmk990212@naver.com>
2023-02-02 21:49:32 +0900

    02/02

--- 46th/20.Inheritance/20.Inheritance.cpp
+++ 46th/20.Inheritance/20.Inheritance.cpp
@@ -10,40 +10,40 @@
 //상속을 쓰지않고 같은 효과를 내는방법
 //포함관계
 //겹치는 부분의 코드들을 따로 구조체를 만들어서 맴버변수로 만들면됨
-struct Stat{
+struct Stat {
     int level;
     int hp;
-}
+};
 
 
-class GameObject{
-    
-private:    
+class GameObject {
+
+private:
     Stat stat;
-//기본적으로 클래스 외부에서는 사용이 불가능 하지만
-//상속받은 자식들은 사용이 가능함
+    //기본적으로 클래스 외부에서는 사용이 불가능 하지만
+    //상속받은 자식들은 사용이 가능함
 protected:
     bool death;
 public:
     void Damaged()
     {
-        state.hp-=10;   
+        GameObject::stat.hp -= 10;
     }
-    
-    GameObject(){
-        
+
+    GameObject() {
+
     }
-    
-    ~GameObject(){
-        
+
+    virtual ~GameObject() {
+
     }
     //가상 함수를 선언 해주면 이 함수가 호출되면 자식의 같은 이름의 함수로 이동함
-    virtual void shout(){
-        
+    virtual void shout() {
+
     }
-    
-    
-}
+
+
+};
 
 
 //상속
@@ -51,51 +51,51 @@ public:
 struct Player : public GameObject
 {
     //int level;
-    
+
     //int hp;
     int mp;
     int stamina;
 public:
     void shout() override
     {
-        cout<<"i am player";
+        std::cout << "i am player";
     }
-    
-    
-}
 
-struct Monster{
+
+};
+
+struct Monster : public GameObject {
     //int level;
-    
+
     //int hp;
     int stamina;
-        void shout() override
-        {
-        cout<<"i am Monster";
+    void shout() override
+    {
+        std::cout << "i am Monster";
     }
-}
+};
 
 //private로도 상속이 가능함
 //보통 그냥 public으로 
-class NPC: public GameOnject{
-    void Dead(){
-    //모두 private로 들어오게됨
-        death=false;
+class NPC : public GameObject {
+    void Dead() {
+        //모두 private로 들어오게됨
+        death = false;
     }
 public:
-    NPC(){
-    
+    NPC() {
+
     }
-    
-    ~NPC(){
+
+    ~NPC() {
     }
-    
+
     void shout() override
     {
-        cout<<"i am Monster";
+        std::cout << "i am Monster";
     }
-    
-}
+
+};
 
 //상속은 변수 뿐만 아니라 함수까지 가져가고 싶을 때
 //코드를 여러번 쓰지 않고 한번만 써서 매우 편리하게 나옴
@@ -125,7 +125,7 @@ int main()
     Monster* M =new Monster;
     Player* p =new Player;
     
-    GameObject* allObject[3]={npc, M, P}
+    GameObject* allObject[3] = { npc, M, p };
     
     //상속을 이용하면 동시다발적인 처리를 사용이 가능함
     for(int i=0;i<3;i++){
@@ -142,6 +142,13 @@ int main()
     //패딩이 이루어져서 메모리의 크기는 약간 차이날수 있음
     //가상함수 테이블에 저장되어있는 자식들의 함수 주소값을 이용해서 가상함수가 호출되면 맞는 주소를 찾아가서 그 함수를 
     
+    //GameoBject형의 메모리를 삭제하기 때문에
+    //제대로 삭제가 안됨
+    for (int i = 0; i < 3; i++) {
+        delete allObject[i];
+    }
+    //하위에 monster나 player들은 Gameobject를 상속받았기 때문에
+    //소멸자도 가상함수로 만들어서 메모리 해제
     
     return 0;
 }
